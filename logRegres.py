f17c86b6a4ce9024b231f32eb052e7784e64fdf4
zhongliue <liuezhong@163.com>
2017-08-26 21:59:36 +0800

    Logistic回归

--- logRegres.py
+++ logRegres.py
@@ -4,6 +4,7 @@ Logistic Regression Working Module
 @author: Peter
 '''
 from numpy import *
+import matplotlib.pyplot as plt
 
 def loadDataSet():
     dataMat = []; labelMat = []
@@ -66,7 +67,7 @@ def stocGradAscent1(dataMatrix, classLabels, numIter=150):
     m,n = shape(dataMatrix)
     weights = ones(n)   #initialize to all ones
     for j in range(numIter):
-        dataIndex = range(m)
+        dataIndex = list(range(m))
         for i in range(m):
             alpha = 4/(1.0+j+i)+0.0001    #apha decreases with iteration, does not 
             randIndex = int(random.uniform(0,len(dataIndex)))#go to 0 because of the constant
@@ -102,12 +103,12 @@ def colicTest():
         if int(classifyVector(array(lineArr), trainWeights))!= int(currLine[21]):
             errorCount += 1
     errorRate = (float(errorCount)/numTestVec)
-    print "the error rate of this test is: %f" % errorRate
+    print("the error rate of this test is: %f" % errorRate)
     return errorRate
 
 def multiTest():
     numTests = 10; errorSum=0.0
     for k in range(numTests):
         errorSum += colicTest()
-    print "after %d iterations the average error rate is: %f" % (numTests, errorSum/float(numTests))
+    print("after %d iterations the average error rate is: %f" % (numTests, errorSum/float(numTests)))
         
---
+++ mytry.py
@@ -0,0 +1,8 @@
+import logRegres
+from numpy import *
+
+# dataMat,labelMat=logRegres.loadDataSet()
+# weights = logRegres.stocGradAscent1(array(dataMat),labelMat)
+# logRegres.plotBestFit(weights)
+
+logRegres.multiTest()
