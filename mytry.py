c77c51e45fa30a978fa3c104bdb865bdba6adeab
Administrator <liuezhong@163.com>
2017-08-28 09:46:07 +0800

    Logistic回归

--- mytry.py
+++ mytry.py
@@ -1,6 +1,19 @@
 import logRegres
 from numpy import *
 
+dataArr,labelMat = logRegres.loadDataSet()
+# print(dataArr)
+# print(labelMat)
+# print(logRegres.gradAscent(dataArr, labelMat))
+
+# 批梯度上升算法
+# weights = logRegres.gradAscent(dataArr, labelMat)
+# logRegres.plotBestFit(weights.getA())
+
+# 随机梯度上升算法
+weights = logRegres.stocGradAscent0(array(dataArr), labelMat)
+logRegres.plotBestFit(weights)
+
 # dataMat,labelMat=logRegres.loadDataSet()
 # weights = logRegres.stocGradAscent1(array(dataMat),labelMat)
 # logRegres.plotBestFit(weights)
